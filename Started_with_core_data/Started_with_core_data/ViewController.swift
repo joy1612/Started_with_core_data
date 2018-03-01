760aca2d504da763489211d304133c039874231d
Jay Zalawadia <jayzalawadia@jays-macbook-pro-2.local>
2018-03-01 16:51:47 -0500

    Done with core data

---
+++ Started_with_core_data/Started_with_core_data/ViewController.swift
@@ -0,0 +1,136 @@
+//
+//  ViewController.swift
+//  Started_with_core_data
+//
+//  Created by Jay Zalawadia on 3/1/18.
+//  Copyright © 2018 Jay Zalawadia. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+
+    @IBOutlet weak var tableview: UITableView!
+    
+    //var names: [String] = []
+    var people: [NSManagedObject] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "The List"
+        tableview.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "cell")
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        //2
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        //3
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    @IBAction func add(_ sender: Any) {
+        let alert = UIAlertController(title: "New Name",
+                                      message: "Add a new name",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) {
+                                        [unowned self] action in
+                                        
+                                        guard let textField = alert.textFields?.first,
+                                            let nameToSave = textField.text else {
+                                                return
+                                        }
+                                        
+                                        //self.names.append(nameToSave)
+                                        self.save(name: nameToSave)
+                                        self.tableview.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default)
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
+    func save(name: String) {
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Person",
+                                       in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        // 3
+        person.setValue(name, forKeyPath: "name")
+        
+        // 4
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =
+            tableView.dequeueReusableCell(withIdentifier: "cell",
+                                          for: indexPath)
+        let person = people[indexPath.row]
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+
---
+++ Started_with_core_data/Started_with_core_dataTests/Started_with_core_dataTests.swift
@@ -0,0 +1,36 @@
+//
+//  Started_with_core_dataTests.swift
+//  Started_with_core_dataTests
+//
+//  Created by Jay Zalawadia on 3/1/18.
+//  Copyright © 2018 Jay Zalawadia. All rights reserved.
+//
+
+import XCTest
+@testable import Started_with_core_data
+
+class Started_with_core_dataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
